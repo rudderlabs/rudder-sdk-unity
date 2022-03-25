389f4e5996dcda5e2378300e6f80481ccf0df3c5
Desu Sai Venkat <saivenkatdesu@gmail.com>
2022-03-25 21:48:36 +0530

    Updated Android and iOS SDK's in Plugins Folder

--- Plugins/Android/unity-plugin/src/main/java/com/rudderstack/android/sdk/core/RudderLibraryInfo.java
+++ Plugins/Android/unity-plugin/src/main/java/com/rudderstack/android/sdk/core/RudderLibraryInfo.java
@@ -1,11 +1,10 @@
 package com.rudderstack.android.sdk.core;
 
 import com.google.gson.annotations.SerializedName;
-import com.rudderstack.android.sdk.BuildConfig;
 
 class RudderLibraryInfo {
     @SerializedName("name")
     private String name = BuildConfig.LIBRARY_PACKAGE_NAME;
     @SerializedName("version")
-    private String version = "1.2.1";
+    private String version = "1.5.0";
 }
